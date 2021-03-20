bcb9374f9bd462e8250a24386cb835e207ee76b9
zu1k <zu1k@users.noreply.github.com>
2021-03-20 09:05:46 +0000

    deploy: 30af2ba4874e9b9b83c7948e91145544d2166666

--- sw.js
+++ sw.js
@@ -1,28 +1,31 @@
-const cdn = 'blog.zuik.ren'
+importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.3/workbox/workbox-sw.js');
+workbox.setConfig({
+    modulePathPrefix: 'https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.3/workbox/'
+});
+const { core, routing, strategies, expiration } = workbox;
+const { ExpirationPlugin } = expiration;
+const { CacheFirst, NetworkFirst } = strategies;
+core.skipWaiting();
+core.clientsClaim();
 
-async function requestWillFetch(request) {
-    let url = new URL(request.url);
-    url.protocol = 'https';
-    url.host = cdn;
-    url.port = '';
-
-    let method = request.method;
-    let headers = new Headers(request.headers);
-    headers.set('Host', cdn);
-    headers.set('Referer', url.href);
-
-    let response = await fetch(url.href, {
-        method: method,
-        headers: headers
+routing.registerRoute(
+    /.*cdn\.jsdelivr\.net/,
+    new CacheFirst({
+        cacheName: 'static-immutable',
+        fetchOptions: {
+            mode: 'cors',
+            credentials: 'omit'
+        },
+        plugins: [
+            new ExpirationPlugin({
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+                purgeOnQuotaError: true
+            })
+        ]
     })
+);
 
-    let status = response.status;
-    return new Response(response.body, {
-        status,
-        headers: response.headers
-    });
-}
-
+const cdn = 'blog.zuik.ren'
 const myPlugin = {
     requestWillFetch: async ({request, event, state}) => {
         var url = new URL(request.url);
@@ -44,33 +47,17 @@ const myPlugin = {
     }
 };
 
-importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@3.6.3/workbox/workbox-sw.js');
-if (workbox) {
-    workbox.setConfig({
-    modulePathPrefix: 'https://cdn.jsdelivr.net/npm/workbox-cdn@3.6.3/workbox/'
-    });
+const myHandler = new CacheFirst({
+    cacheName: 'cache-01',
+    plugins: [
+        myPlugin
+    ]
+});
 
-
-    const urlHandler = new workbox.strategies.CacheFirst({
-        cacheName: 'cache-01',
-        plugins: [
-            myPlugin
-        ]
-    });
-
-    workbox.routing.registerRoute(/cdn\.jsdelivr\.net/, 
-    workbox.strategies.cacheFirst({
-        cacheName: 'static-lib-01',
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 30 * 24 * 60 * 60,
-            }),
-            new workbox.cacheableResponse.Plugin({
-                statuses: [0, 200],
-            }),
-        ]    
+routing.registerRoute( /.*lgf\.im/, myHandler)
+routing.registerRoute( /localhost.*/, myHandler)
+routing.setDefaultHandler(
+    new NetworkFirst({
+        networkTimeoutSeconds: 3
     })
-    )
-    workbox.routing.registerRoute( /:\/\/lgf\.im\//, urlHandler)
-    workbox.routing.registerRoute( /localhost.*/, urlHandler)
-}
+);
