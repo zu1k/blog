ec6f2cf9cf61e45a808fbdef3b51bc17ccadfa3e
zu1k <zu1k@users.noreply.github.com>
2021-06-10 13:04:15 +0000

    deploy: b5b6cae09748b82e0613308aae3f4efff9216828

--- reg.js
+++ reg.js
@@ -7,20 +7,3 @@ if ('serviceWorker' in navigator) {
         });
     });
 }
-
-function webpushrscript() {
-    function webpushr1(w,d, s, id) {
-        if(typeof(w.webpushr)!=='undefined') 
-            return;
-        w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};
-        var js, fjs = d.getElementsByTagName(s)[0];
-        js = d.createElement(s); 
-        js.id = id;
-        js.async=1;
-        js.src = "https://cdn.webpushr.com/app.min.js";
-        fjs.parentNode.appendChild(js);
-    };
-    webpushr1(window, document, 'script', 'webpushr-jssdk');
-    webpushr('setup',{'key':'BCIE9s5vikfzfIqnC164D6-Gwquv1Wl38cCZOj8TzG4ZvPns4u1NWzbEb8rRvF3mWZJ8sJzhhTvs0o--sLFmUyQ' });
-};
-setTimeout(webpushrscript, 10000);
